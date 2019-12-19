0faf204cebe129a54d8c651912f4ecdcd62b6b52
subarna8 <mahmuda.akhter019@gmail.com>
2019-12-19 00:45:47 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+let Mahmuda {
+  name: "Mahmuda Akhter",
+  age: "24",
+  address: "House#120, Road#12, Block#G, South Bonasree, Dhaka-1219",
+  school: "Scholars School",
+  college: "Bangladesh International School and college",
+  university: "IUB"
+}
+
+let Mahua {
+  name: "Mahua Akhter",
+  age: "18",
+  address: "House#120, Road#12, Block#G, South Bonasree, Dhaka-1219",
+  school: "Willes Little Flower School",
+  college: "Academia",
+  university: "IUB"
+}
+
+
+
+
+function App () {
+  return (
+    <div >
+      Academic info 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CV.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function CV (props) {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
