e37412f123410bd66ec2bfcab4b098d3ba1bc58d
subarna8 <mahmuda.akhter019@gmail.com>
2019-12-24 13:39:30 +0600

    cv on progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from 'react'
+ import './App.css';
 // import React from 'react';
-// import CV from './CV';
+
+ import CV from './CV';
 // //import logo from './logo.svg';
 // import './App.css';
 
@@ -36,23 +39,24 @@
 
 // export default App;
 
-import React from 'react';
-
-//import Props from './Props'
-import CV from './CV'
-import './App.css';
+// import React from 'react'
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello there!!!</h1>
-//       <Props name = {"Subarna"}/>
-//       <Props name = {"Sara"}/>
-//     </div>
-//   );
-// }
-let Mahmuda = {
+// //import Props from './Props'
+// import CV from './CV'
+// import './App.css';
+
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <h1>Hello there!!!</h1>
+// //       <Props name = {"Subarna"}/>
+// //       <Props name = {"Sara"}/>
+// //     </div>
+// //   );
+// // }
+ let Mahmuda = {
   name: "Mahmuda Akhter",
   age: "24",
   address: "House#140, Road#12, Block#G, South Bonasree, Dhaka-1219",
@@ -61,22 +65,94 @@ let Mahmuda = {
   university: "IUB"
 }
 
-let Arpona = {
-  name: "Mahua Akhter",
-  age: "19",
-  address: "House#140, Road#12, Block#G, South Bonasree, Dhaka-1219",
-  school: "Willes Little Flower School",
-  college: "Academia College",
-  university: "IUB"
+// // let Arpona = {
+// //   name: "Mahua Akhter",
+// //   age: "19",
+// //   address: "House#140, Road#12, Block#G, South Bonasree, Dhaka-1219",
+// //   school: "Willes Little Flower School",
+// //   college: "Academia College",
+// //   university: "IUB"
+// // }
+
+// export default class App extends React.Component () {
+//   constructor(props){
+//     super(props);
+//   }
+//   render(){
+//     return (
+//       <div>
+//         <div>
+//         {/* <h3> Name: <input placeholder = 'Name' value = {this.state.name} onChange = {this.handleChange} ></input></h3> */}
+//       
+//         <CV info = {Mahmuda}/>
+//         </div>
+        
+//       </div>
+//     )
+//   }
+  
+// }
+
+ 
+
+///////////////////////////////////
+
+export default class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: 'name',
+            age: 'age',
+            address: 'address',
+            show: false
+        }
+    }
+    handleChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            name: event.target.value,
+            show : false
+
+        })
+    }
+
+    ageChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            age: event.target.value,
+            show : false
+
+        })
+    }
+
+    addressChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            address: event.target.value,
+            show : false
+
+        })
+    }
+
+    buttonClicked = (event) => {
+        this.setState({
+            show : true
+        })
+
+    }
+    render()
+    {
+        console.log("hi")
+        return(
+          <div>
+              <h3> Name: <input placeholder = 'Name'  onChange  ></input></h3>
+              <CV info={Mahmuda} />
+          </div>
+           
+        )
+    }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <CV info = {Mahmuda}/>
-      <CV info = {Arpona} />
-    </div>
-  );
-}
 
-export default App
+
+///////////////////////////////////
--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,9 @@ export default class Form extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            name: 'Anything',
+            name: 'name',
+            age: 'age',
+            address: 'address',
             show: false
         }
     }
@@ -15,6 +17,25 @@ export default class Form extends React.Component{
 
         })
     }
+
+    ageChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            age: event.target.value,
+            show : false
+
+        })
+    }
+
+    addressChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            address: event.target.value,
+            show : false
+
+        })
+    }
+
     buttonClicked = (event) => {
         this.setState({
             show : true
@@ -26,9 +47,14 @@ export default class Form extends React.Component{
         console.log("hi")
         return(
             <div>
-                <div> Name: <input placeholder = 'Name' value = {this.state.name} onChange = {this.handleChange} ></input></div>
+                <h1>Personal Info</h1>
+                <h3> Name: <input placeholder = 'Name' value = {this.state.name} onChange = {this.handleChange} ></input></h3>
+                <h3> age: <input placeholder = 'age' value = {this.state.age} onChange = {this.ageChange} ></input></h3>
+                <h3> address: <input placeholder = 'address' value = {this.state.address} onChange = {this.addressChange} ></input></h3>
                 <button onClick = {this.buttonClicked} >Click Button</button>
-                <div>My name is:</div> {this.state.show && <div>{this.state.name}</div>}
+                <h3>My name is:</h3> {this.state.show && <div>{this.state.name}</div>}
+                <h3>My age is:</h3> {this.state.show && <div>{this.state.age}</div>}
+                <h3>My address is:</h3> {this.state.show && <div>{this.state.address}</div>}
             </div>
         )
     }
